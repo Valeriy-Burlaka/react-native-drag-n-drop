8b74235d640c3278356bf064c65bf30431eb9d27
Valeriy Burlaka <valeriy.burlaka88@gmail.com>
2023-02-17 13:40:46 +0100

    Put the words into their initial position at Placehodler

--- app/components/SortableWord.tsx
+++ app/components/SortableWord.tsx
@@ -11,7 +11,10 @@ import { PanGestureHandler } from "react-native-gesture-handler";
 import { between, useVector } from "react-native-redash";
 
 import { calculateLayout, lastOrder, Offset, reorder } from "./Layout";
-import Placeholder, { MARGIN_TOP, MARGIN_LEFT } from "./Placeholder";
+import Placeholder, {
+  MARGIN_TOP as PLACEHOLDER_TOP_MARGIN,
+  MARGIN_LEFT as PLACEHOLDER_LEFT_MARGIN,
+} from "./Placeholder";
 
 interface SortableWordProps {
   offsets: Offset[];
@@ -27,13 +30,25 @@ export const SortableWord = ({
   containerWidth,
 }: SortableWordProps) => {
   const offset = offsets[index];
+  const isInBank = useDerivedValue(() => offset?.order.value === -1);
+  const translateX = useDerivedValue(() => {
+    return isInBank.value ? offset?.originalX.value - PLACEHOLDER_LEFT_MARGIN : offset?.x.value;
+  });
+  const translateY = useDerivedValue(() => {
+    return isInBank.value ? offset?.originalY.value + PLACEHOLDER_TOP_MARGIN : offset?.y.value;
+  });
+
   const style = useAnimatedStyle(() => {
     return {
       position: "absolute",
       top: 0,
       left: 0,
-      width: offset.width.value,
-      height: offset.height.value,
+      width: offset?.width.value,
+      height: offset?.height.value,
+      transform: [
+        { translateX: translateX.value },
+        { translateY: translateY.value },
+      ]
     };
   });
   return (
